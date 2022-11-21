c34ff7fd53598bed8a2a59d5801935cf647c738c
petertau <peter.tau1999@gmail.com>
2022-11-21 10:39:33 +0800

    Update Notificaiton

--- lib/screens/MovieDetail.dart
+++ lib/screens/MovieDetail.dart
@@ -29,7 +29,7 @@ class MovieDetail extends StatelessWidget {
                           alignment: Alignment.center,
                           child: Image.network(
                             snapshot.data!.poster,
-                            width: 200,
+                            width: 180,
                           ),
                         ),
                         Text(snapshot.data!.plot, textAlign: TextAlign.justify),
--- lib/screens/MovieHomePage.dart
+++ lib/screens/MovieHomePage.dart
@@ -35,49 +35,78 @@ class MovieHomePageState extends State<MovieHomePage> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        appBar: AppBar(
-          title: const Center(child: Text('Search Movies')),
-        ),
-        body: Column(
-          children: <Widget>[
-            Container(
-              padding: const EdgeInsets.all(10),
-              child: Row(children: <Widget>[
-                Flexible(
-                  child: TextField(
-                    controller: searchTextController,
-                    decoration:
-                        const InputDecoration(hintText: 'Enter a search term'),
+    return WillPopScope(
+      onWillPop: () => _onBackButtonPress(context),
+      child: Scaffold(
+          appBar: AppBar(
+            title: const Center(child: Text('Search Movies')),
+          ),
+          body: Column(
+            children: <Widget>[
+              Container(
+                padding: const EdgeInsets.all(10),
+                child: Row(children: <Widget>[
+                  Flexible(
+                    child: TextField(
+                      controller: searchTextController,
+                      decoration: const InputDecoration(
+                          hintText: 'Enter a search term'),
+                    ),
                   ),
-                ),
-                IconButton(
-                  icon: const Icon(Icons.search),
-                  tooltip: 'Search Movies',
-                  onPressed: () {
-                    setState(() {
-                      searchText = searchTextController.text;
-                      SystemChannels.textInput.invokeMethod('TextInput.hide');
-                    });
-                  },
-                ),
-              ]),
-            ),
-            if (searchText.isNotEmpty)
-              FutureBuilder<List<Movie>>(
-                  future: searchMovies(searchText),
-                  builder: (context, snapshot) {
-                    if (snapshot.hasData) {
-                      return Expanded(
-                          child: MovieList(
-                              movies: snapshot.data??[],
-                              itemClick: itemClick));
-                    } else if (snapshot.hasError) {
-                      return Text("${snapshot.error}");
-                    }
-                    return const CircularProgressIndicator();
-                  }),
-          ],
-        ));
+                  IconButton(
+                    icon: const Icon(Icons.search),
+                    tooltip: 'Search Movies',
+                    onPressed: () {
+                      setState(() {
+                        searchText = searchTextController.text;
+                        SystemChannels.textInput.invokeMethod('TextInput.hide');
+                      });
+                    },
+                  ),
+                ]),
+              ),
+              if (searchText.isNotEmpty)
+                FutureBuilder<List<Movie>>(
+                    future: searchMovies(searchText),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        return Expanded(
+                            child: MovieList(
+                                movies: snapshot.data ?? [],
+                                itemClick: itemClick));
+                      } else if (snapshot.hasError) {
+                        return Text("${snapshot.error}");
+                      }
+                      return const Center(child: CircularProgressIndicator());
+                    }),
+            ],
+          )),
+    );
   }
+}
+
+Future<bool> _onBackButtonPress(BuildContext context) async {
+  bool exitApp = await showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text("Confirmation"),
+          content: const Text("Do you want to close the app?"),
+          actions: <Widget>[
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(true);
+              },
+              child: const Text("Yes"),
+            ),
+            TextButton(
+              onPressed: () {
+                Navigator.of(context).pop(false);
+              },
+              child: const Text("No"),
+            ),
+          ],
+        );
+      });
+      return exitApp;
 }
